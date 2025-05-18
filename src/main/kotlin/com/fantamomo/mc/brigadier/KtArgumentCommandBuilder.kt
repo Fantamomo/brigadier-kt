490c1ea781efbc0b6cc0ac4f7ced67385eae3f4d
Fantamomo <git@fantamomo.com>
2025-05-18 17:34:31 +0200

    feat: add generic argument support to KtArgumentCommandBuilder

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtArgumentCommandBuilder.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtArgumentCommandBuilder.kt
@@ -1,4 +1,8 @@
 package com.fantamomo.mc.brigadier
 
-class KtArgumentCommandBuilder {
+import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
+
+class KtArgumentCommandBuilder<S, T>(name: String, type: ArgumentType<T>) : KtCommandBuilder<S, RequiredArgumentBuilder<S, T>>() {
+    override val builder: RequiredArgumentBuilder<S, T> = RequiredArgumentBuilder.argument(name, type)
 }
