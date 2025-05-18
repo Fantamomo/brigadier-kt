40c87a479ed56d4e6ac1f025b868d37692849ed0
Fantamomo <git@fantamomo.com>
2025-05-18 17:37:28 +0200

    feat: add argument handling extensions to `KtCommandContext`
    - Implement `arg` and `optionalArg` functions for argument retrieval.
    - Add `SINGLE_SUCCESS` property for command success constant.
    - Introduce reified inline variants for type-safe argument access.

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtCommandContext.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtCommandContext.kt
@@ -1,4 +1,21 @@
 package com.fantamomo.mc.brigadier
 
-class KtCommandContext {
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.context.CommandContext
+import kotlin.reflect.KClass
+
+@Suppress("UnusedReceiverParameter")
+val CommandContext<*>.SINGLE_SUCCESS: Int get() = Command.SINGLE_SUCCESS
+
+fun <T : Any> CommandContext<*>.arg(kclass: KClass<T>, name: String): T =
+    this.getArgument(name, kclass.javaPrimitiveType ?: kclass.java)
+
+fun <T : Any> CommandContext<*>.optionalArg(kclass: KClass<T>, name: String): T? = try {
+    arg(kclass, name)
+} catch (e: IllegalArgumentException) {
+    null
 }
+
+inline fun <reified T : Any> CommandContext<*>.arg(name: String): T = arg(T::class, name)
+
+inline fun <reified T : Any> CommandContext<*>.optionalArg(name: String): T? = optionalArg(T::class, name)
