948efe0d3c0b6901c64f086c7ccb1551c089594d
Fantamomo <git@fantamomo.com>
2025-05-18 17:34:07 +0200

    feat: introduce abstract KtCommandBuilder with DSL annotation and node building

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtCommandBuilder.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtCommandBuilder.kt
@@ -1,4 +1,11 @@
 package com.fantamomo.mc.brigadier
 
-class KtCommandBuilder {
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.tree.CommandNode
+
+@KtCommandDsl
+abstract class KtCommandBuilder<S, B : ArgumentBuilder<S, B>> {
+    internal abstract val builder: B
+
+    fun build(): CommandNode<S> = builder.build()
 }
