9f93f78eece6f8de3121abeccab717b354fac4b7
Fantamomo <git@fantamomo.com>
2025-05-18 17:35:18 +0200

    feat: add `command` function for building command nodes

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtBrigadier.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtBrigadier.kt
@@ -1,2 +1,9 @@
 package com.fantamomo.mc.brigadier
 
+import com.mojang.brigadier.tree.CommandNode
+
+fun <S> command(literal: String, block: KtLiteralCommandBuilder<S>.() -> Unit): CommandNode<S> {
+    val builder = KtLiteralCommandBuilder<S>(literal)
+    builder.block()
+    return builder.build()
+}
