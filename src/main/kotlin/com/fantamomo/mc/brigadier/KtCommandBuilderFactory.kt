63c858c366b346ed11e86c606205565544c09ed1
Fantamomo <git@fantamomo.com>
2025-05-18 17:37:42 +0200

    feat: add utility functions for command builder literals and arguments
    - Introduced `literal` function to simplify creating literal command builders.
    - Added `argument` function to streamline defining argument-based command builders.

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtCommandBuilderFactory.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtCommandBuilderFactory.kt
@@ -1,2 +1,15 @@
 package com.fantamomo.mc.brigadier
 
+import com.mojang.brigadier.arguments.ArgumentType
+
+fun <S> KtCommandBuilder<S, *>.literal(literal: String, block: KtLiteralCommandBuilder<S>.() -> Unit) {
+    val literalBuilder = KtLiteralCommandBuilder<S>(literal)
+    literalBuilder.block()
+    builder.then(literalBuilder.build())
+}
+
+fun <S, T> KtCommandBuilder<S, *>.argument(name: String, type: ArgumentType<T>, block: KtArgumentCommandBuilder<S, T>.() -> Unit) {
+    val argumentBuilder = KtArgumentCommandBuilder<S, T>(name, type)
+    argumentBuilder.block()
+    builder.then(argumentBuilder.build())
+}
