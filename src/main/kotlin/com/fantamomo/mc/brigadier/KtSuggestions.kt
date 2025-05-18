1327398386290b33bb2da97583d92f932d026e88
Fantamomo <git@fantamomo.com>
2025-05-18 17:37:51 +0200

    feat: add support for custom command suggestions in KtSuggestions
    - Introduce `KtSuggestionsBuilder` for building suggestions.
    - Add functions for string suggestions with and without tooltips.
    - Implement `suggests` and `suggestsOverride` for flexible suggestion handling.

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtSuggestions.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtSuggestions.kt
@@ -1,2 +1,32 @@
 package com.fantamomo.mc.brigadier
 
+import com.mojang.brigadier.Message
+import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.suggestion.Suggestions
+import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import java.util.concurrent.CompletableFuture
+
+@KtCommandDsl
+class KtSuggestionsBuilder<S>(val context: CommandContext<S>, val builder: SuggestionsBuilder)
+
+fun KtSuggestionsBuilder<*>.suggest(suggestion: String) {
+    builder.suggest(suggestion)
+}
+
+fun KtSuggestionsBuilder<*>.suggest(suggestion: String, tooltip: Message) {
+    builder.suggest(suggestion, tooltip)
+}
+
+fun <S> KtArgumentCommandBuilder<S, *>.suggests(block: KtSuggestionsBuilder<S>.() -> Unit) {
+    builder.suggests { context, builder ->
+        val suggestions = KtSuggestionsBuilder(context, builder)
+        suggestions.block()
+        suggestions.builder.buildFuture()
+    }
+}
+
+fun <S> KtArgumentCommandBuilder<S, *>.suggestsOverride(
+    block: @KtCommandDsl CommandContext<S>.(builder: SuggestionsBuilder) -> CompletableFuture<Suggestions>,
+) {
+    builder.suggests(block)
+}
