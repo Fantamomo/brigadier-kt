e1d55b91033a34770947f4618cd47873ca6969ae
Fantamomo <git@fantamomo.com>
2026-02-09 14:36:29 +0100

    feat: add `runGuards` parameter to `execute` for conditional guard execution
    - Allow skipping guard execution in `execute` by setting `runGuards` to `false`.
    - Update method implementation and documentation to reflect the new behavior.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/mc/brigadier/KtExecution.kt
+++ src/main/kotlin/com/fantamomo/mc/brigadier/KtExecution.kt
@@ -5,21 +5,28 @@ import com.mojang.brigadier.context.CommandContext
 /**
  * Registers the execution logic for this command node.
  *
- * Before invoking the provided [block], all guards in the
+ * If [runGuards] is `true` (the default), the execution logic is as followed:
+ *
+ * - Before invoking the provided [block], all guards in the
  * current execution path are executed.
- *
- * If a guard returns [GuardResult.Abort], execution stops and the
+ * - If a guard returns [GuardResult.Abort], execution stops and the
  * provided result code is returned to Brigadier.
- *
- * If all guards return [GuardResult.Continue], the execution block
+ * - If all guards return [GuardResult.Continue], the execution block
  * is invoked.
  *
+ * If [runGuards] is `false`, the provided [block] is executed without executing any guard.
+ *
  * @param block The command execution logic.
+ * @param runGuards Whether to run guards before executing the block.
  *
  * @author Fantamomo
  * @since 1.0-SNAPSHOT
  */
-fun <S> KtCommandBuilder<S, *>.execute(block: @KtCommandDsl CommandContext<S>.() -> Int) {
+fun <S> KtCommandBuilder<S, *>.execute(runGuards: Boolean = true, block: @KtCommandDsl CommandContext<S>.() -> Int) {
+    if (!runGuards) {
+        builder.executes(block)
+        return
+    }
     val guards = guards
     builder.executes { ctx ->
         if (!guards.hasCustomGuard()) return@executes block(ctx)
